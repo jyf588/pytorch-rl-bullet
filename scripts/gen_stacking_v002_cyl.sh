188efe438d5927611db2a5f9534a506552fa9426
Michelle Guo <shellguo95@gmail.com>
2020-03-13 13:18:28 -0700

    Update bash scripts and tables.

--- scripts/gen_stacking_v002_cyl.sh
+++ scripts/gen_stacking_v002_cyl.sh
@@ -2,7 +2,7 @@ python enjoy.py \
     --env-name InmoovHandPlaceBulletEnv-v8 \
     --load-dir trained_models_0302_cyl_4_place_0307_2/ppo/ \
     --non-det 0 \
-    --seed=1898 \
+    --seed=0 \
     --random_shape 0 \
     --random_size 1 \
     --default_box 0 \
@@ -13,4 +13,4 @@ python enjoy.py \
     --control_skip 6 \
     --obs_noise 1 \
     --gen_vision_dataset 1 \
-    --n_trials 1000
+    --n_trials 460
