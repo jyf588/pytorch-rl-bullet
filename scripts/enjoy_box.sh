014e5f628ec4edc2e4738c9c277c013cbf00c677
Michelle Guo <shellguo95@gmail.com>
2020-03-12 23:02:07 -0700

    Various bash scripts and txt files.

---
+++ scripts/enjoy_box.sh
@@ -0,0 +1,13 @@
+python enjoy.py \
+    --env-name InmoovHandPlaceBulletEnv-v7 \
+    --load-dir trained_models_0302_box_20_place_0307_1/ppo/ \
+    --non-det 0 \
+    --seed=1898 \
+    --random_shape 0 \
+    --random_size 1 \
+    --default_box 1 \
+    --up 1 \
+    --renders 0 \
+    --exclude_hard 0  \
+    --obs_noise 1 \
+    --n_trials 1000
---
+++ scripts/enjoy_box_v6.sh
@@ -0,0 +1,11 @@
+python enjoy.py \
+    --env-name InmoovHandPlaceBulletEnv-v6 \
+    --load-dir trained_models_0219_box_2_place_0220_3/ppo/ \
+    --non-det 0 \
+    --seed=1897 \
+    --random_shape 0 \
+    --random_size 1 \
+    --default_box 1 \
+    --up 1 \
+    --renders 0 \
+    --exclude_hard 0 
