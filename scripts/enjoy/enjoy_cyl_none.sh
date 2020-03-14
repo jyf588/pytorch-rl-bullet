9540e43c039dd8efd01a15fb87023892d347e124
Michelle Guo <shellguo95@gmail.com>
2020-03-14 10:24:08 -0700

    Add bash scripts for no pose and saving states.

---
+++ scripts/enjoy/enjoy_cyl_none.sh
@@ -0,0 +1,14 @@
+python enjoy.py \
+    --env-name InmoovHandPlaceBulletEnv-v8 \
+    --load-dir trained_models_0302_cyl_4_place_0312_np_0/ppo/ \
+    --non-det 0 \
+    --seed=1898 \
+    --random_shape 0 \
+    --random_size 1 \
+    --default_box 0 \
+    --up 1 \
+    --renders 0 \
+    --exclude_hard 0 \
+    --obs_noise 1 \
+    --use_gt_6d 0 \
+    --n_trials 1000
