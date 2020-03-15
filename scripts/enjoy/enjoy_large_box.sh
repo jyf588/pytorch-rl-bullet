e70fe8e90c679f12c2ab6ef277370e2f4b399551
Michelle Guo <shellguo95@gmail.com>
2020-03-15 11:45:44 -0700

    Update bash scripts.

--- scripts/enjoy/enjoy_large_box.sh
+++
@@ -1,6 +0,0 @@
-time python enjoy.py \
-    --env-name InmoovHandPlaceBulletEnv-v6 \
-    --load-dir ~/policies/enjoy_models/init_xyz_up/large_box/ppo/ \
-    --non-det 0 \
-    --n_trials 1 \
-    --renders 1
