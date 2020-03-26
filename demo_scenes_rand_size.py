9fc645f6be84b1740aed5f15d2721069ea31c4ce
jyf588 <jiangyifeng588@gmail.com>
2020-03-26 15:52:20 -0700

    add scene new version

---
+++ demo_scenes_rand_size.py
@@ -0,0 +1,76 @@
+SCENES = {
+    1: [
+        {
+            "shape": "box",
+            "color": "yellow",
+            "position": [0.15, 0.7, 0, 0],
+        },
+        {
+            "shape": "box",
+            "color": "green",
+            "position": [0.2, 0.4, 0, 0],
+        },
+        {
+            "shape": "cylinder",
+            "color": "blue",
+            "position": [0.1, -0.05, 0, 0],
+        },
+        {
+            "shape": "box",
+            "color": "yellow",
+            "position": [0.0, 0.1, 0, 0],
+        },
+    ],
+    2: [
+        {
+            "shape": "box",
+            "color": "yellow",
+            "position": [0.15, 0.7, 0, 0],
+            "size": "large",
+        },
+        {
+            "shape": "cylinder",
+            "color": "green",
+            "position": [0.1, -0.06, 0, 0],
+            "size": "large",
+        },
+        {
+            "shape": "cylinder",
+            "color": "blue",
+            "position": [0.2, 0.4, 0, 0],
+            "size": "large",
+        },
+        {
+            "shape": "box",
+            "color": "yellow",
+            "position": [0.3, 0.3, 0, 0],
+            "size": "large",
+        },
+    ],
+    3: [
+        {
+            "shape": "box",
+            "color": "yellow",
+            "position": [0.15, 0.7, 0, 0],
+            "size": "large",
+        },
+        {
+            "shape": "cylinder",
+            "color": "green",
+            "position": [0.1, -0.06, 0, 0],
+            "size": "large",
+        },
+        {
+            "shape": "cylinder",
+            "color": "blue",
+            "position": [0.1, 0.3, 0, 0],
+            "size": "large",
+        },
+        {
+            "shape": "box",
+            "color": "yellow",
+            "position": [0.2, 0.4, 0, 0],
+            "size": "large",
+        },
+    ],
+}
