2ead9feb5fdeb0f15021141f1a27759bf4f9ba3f
Michelle Guo <shellguo95@gmail.com>
2020-05-17 14:40:46 -0700

    Update scene generator to use json files with scene params, reproduce old scene generation.

--- scene/compare_sets.py
+++ scene/compare_sets.py
@@ -1,19 +1,26 @@
 import os
 import ns_vqa_dart.bullet.util as util
 
-data_dir = "/home/mguo/data/dash/t1_small_range_no_zrot_no_sphere"
-set1 = "stack_v1"
-set2 = "stack"
+dir1 = "/home/mguo/data/dash/t1_0513"
+dir2 = "/home/mguo/data/dash/t1"
 
-dir1 = os.path.join(data_dir, set1, "scenes")
-dir2 = os.path.join(data_dir, set2, "scenes")
-for fname1 in os.listdir(dir1):
-    path1 = os.path.join(dir1, fname1)
-    path2 = os.path.join(dir2, fname1)
 
-    x1 = util.load_pickle(path=path1)
-    x2 = util.load_pickle(path=path2)
+for task in ["place", "stack"]:
+    n_diff = 0
+    t1 = os.path.join(dir1, task)
+    t2 = os.path.join(dir2, task)
+    for fname1 in os.listdir(t1):
+        name, _ = fname1.split(".")
+        path1 = os.path.join(t1, fname1)
+        path2 = os.path.join(t2, fname1)
 
-    for idx in range(len(x1)):
-        for attr in ["shape", "color", "radius", "height", "position"]:
-            assert x1[idx][attr] == x2[idx][attr]
+        x1 = util.load_pickle(path=path1)
+        x2 = util.load_pickle(path=path2)
+
+        if x1 != x2:
+            n_diff += 1
+    print(n_diff)
+
+# for idx in range(len(x1)):
+#     for attr in ["shape", "color", "radius", "height", "position", ""]:
+#         assert x1[idx][attr] == x2[idx][attr]
